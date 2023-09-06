7dbde0ada0b4d90342a6c881f65d0322f7abb80c
karol-jedruszek <karol.jedruszek@viatechnik.com>
2023-09-06 14:44:20 +0200

    fix: more documentation changes

---
+++ janet-docs/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('D:\\VTDevelopment\\janet\\janet-docs\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('D:\\VTDevelopment\\janet\\janet-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('D:\\VTDevelopment\\janet\\janet-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css'),
+  require('D:\\VTDevelopment\\janet\\janet-docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('D:\\VTDevelopment\\janet\\janet-docs\\src\\css\\custom.css'),
+];
